833d1e9a042639f644e556d89ce23c9333260a56
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-09-12 10:51:23 -0400

    Wired up userRoutes to userController.

---
+++ controllers/users/userController.js
@@ -0,0 +1,16 @@
+const register = async (req, res) => {
+  try {
+  } catch (error) {}
+}
+
+const login = async (req, res) => {
+  try {
+  } catch (error) {}
+}
+
+const logout = async (req, res) => {
+  try {
+  } catch (error) {}
+}
+
+module.exports = { register, login, logout }
--- routes/users/userRoutes.js
+++ routes/users/userRoutes.js
@@ -1,3 +1,13 @@
 const router = require("express").Router()
 
+const {
+  register,
+  login,
+  logout,
+} = require("../../controllers/users/userController")
+
+router.post("/register", register)
+router.post("/login", login)
+router.get("/logout", logout)
+
 module.exports = router

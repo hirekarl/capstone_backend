f06f19c8d1c1c820d18f75a556e754a92bc04ec1
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-09-12 11:32:52 -0400

    Server correctly handles POST requests to /api/users/register and /api/users/login.

--- controllers/users/userController.js
+++ controllers/users/userController.js
@@ -1,11 +1,78 @@
+const jwt = require("jsonwebtoken")
+
+const User = require("../../models/User")
+
+const { JWT_SECRET, JWT_EXPIRY } = require("../../utils")
+
+const handle500 = (error, res) => {
+  console.error("Internal server error:", error)
+  return res.status(500).json({
+    error: "HTTP 500 Internal Server Error",
+    message: "Couldn't complete the request.",
+  })
+}
+
+const handle409 = (res) => {
+  return res.status(409).json({
+    error: "HTTP 409 Conflict",
+    message: "User with this email address already exists.",
+  })
+}
+
+const handle401 = (res) => {
+  return res.status(401).json({
+    error: "HTTP 401 Unauthorized",
+    message: "Incorrect email address or password.",
+  })
+}
+
 const register = async (req, res) => {
   try {
-  } catch (error) {}
+    const { username, email, password } = req.body
+
+    const foundUser = await User.findOne({ email: email })
+    if (foundUser) return handle409(res)
+
+    const newUser = await User.create({
+      username: username,
+      email: email,
+      password: password,
+    })
+
+    const token = jwt.sign({ data: newUser }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRY,
+    })
+
+    return res.status(201).json({ user: newUser, token: token })
+  } catch (error) {
+    return handle500(error, res)
+  }
 }
 
 const login = async (req, res) => {
   try {
-  } catch (error) {}
+    const { email, password } = req.body
+
+    const foundUser = await User.findOne({ email: email })
+    if (!foundUser) return handle401(res)
+
+    const passwordIsCorrect = await foundUser.isCorrectPassword(password)
+    if (!passwordIsCorrect) return handle401(res)
+
+    const payload = {
+      _id: foundUser._id,
+      username: foundUser.username,
+      email: foundUser.email,
+    }
+
+    const token = jwt.sign({ data: payload }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRY,
+    })
+
+    return res.status(200).json({ token: token, user: foundUser })
+  } catch (error) {
+    return handle500(error, res)
+  }
 }
 
-module.exports = { register, login, logout }
+module.exports = { register, login }

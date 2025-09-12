a34e5c7325ead9650c789e9bf192b2ce5ecdc2e5
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-09-12 11:59:07 -0400

    Added handle500 to utils.

--- utils/index.js
+++ utils/index.js
@@ -5,9 +5,18 @@ const PORT = process.env.PORT
 const JWT_SECRET = process.env.JWT_SECRET
 const JWT_EXPIRY = "2h"
 
+const handle500 = (error, res) => {
+  console.error("Internal server error:", error)
+  return res.status(500).json({
+    error: "HTTP 500 Internal Server Error",
+    message: "Couldn't complete the request.",
+  })
+}
+
 module.exports = {
   MONGO_URI,
   PORT,
   JWT_SECRET,
-  JWT_EXPIRY
+  JWT_EXPIRY,
+  handle500,
 }

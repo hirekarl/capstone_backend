9a86115b97dbebd1b9ac8feb982dcb9e66138a8e
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-09-12 13:50:57 -0400

    Implemented Task model.

---
+++ models/Task.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose")
+
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Task title is required."],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Task description is required."],
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ["To Do", "In Progress", "Done"],
+        message: "{VALUE} is not a valid status.",
+      },
+      default: "To Do",
+    },
+    project: {
+      type: Schema.Types.ObjectId,
+      ref: "Project",
+    },
+  },
+  {
+    toJSON: {
+      transform: (_doc, ret) => {
+        delete ret.__v
+        return ret
+      },
+    },
+  }
+)

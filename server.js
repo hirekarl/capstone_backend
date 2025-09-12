d9009a343018c6198778ea95b38d560f85c8e31d
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-09-12 10:46:25 -0400

    Made root API route.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 
 const connect = require("./db")
+const routes = require("./routes")
 
 const { PORT } = require("./utils")
 
@@ -10,6 +11,8 @@ const run = async () => {
   const app = express()
 
   app.use(express.json())
+
+  app.use("/api", routes)
 
   app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)

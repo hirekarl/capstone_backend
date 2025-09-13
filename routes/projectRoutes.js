8299781f1f8f7bd3b4f36bc9132dcbbf53bc4a61
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-09-13 11:09:37 -0400

    Made task routes nested to /api/projects/:projectId/tasks/.

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -10,10 +10,12 @@ const {
   deleteProject,
 } = require("../controllers/projectController")
 
-const {
-  createTask,
-  viewProjectTasks,
-} = require("../controllers/taskController")
+const taskRoutes = require("./taskRoutes")
+
+// const {
+//   createTask,
+//   viewProjectTasks,
+// } = require("../controllers/taskController")
 
 router.use(authMiddleware)
 
@@ -24,8 +26,10 @@ router.get("/:projectId", userOwnsProject, viewProject)
 router.put("/:projectId", userOwnsProject, editProject)
 router.delete("/:projectId", userOwnsProject, deleteProject)
 
-router.post("/:projectId/tasks", userOwnsProject, createTask)
-router.get("/:projectId/tasks", userOwnsProject, viewProjectTasks)
+router.use("/:projectId/tasks", taskRoutes)
+
+// router.post("/:projectId/tasks", userOwnsProject, createTask)
+// router.get("/:projectId/tasks", userOwnsProject, viewProjectTasks)
 
 // TODO: Consider making all tasks endpoints nested; use mergeParams.
 // see https://expressjs.com/en/guide/routing.html
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,14 +1,23 @@
-const router = require("express").Router()
-
-const { authMiddleware, userOwnsTask } = require("../middleware/auth")
+const router = require("express").Router({ mergeParams: true })
 
 const {
+  authMiddleware,
+  userOwnsProject,
+  userOwnsTask,
+} = require("../middleware/auth")
+
+const {
+  createTask,
+  viewProjectTasks,
   viewTask,
   editTask,
   deleteTask,
 } = require("../controllers/taskController")
 
 router.use(authMiddleware)
+
+router.post("/", userOwnsProject, createTask)
+router.get("/", userOwnsProject, viewProjectTasks)
 
 router.get("/:taskId", userOwnsTask, viewTask)
 router.put("/:taskId", userOwnsTask, editTask)
